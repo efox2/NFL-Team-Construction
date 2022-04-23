dfb8a4e2ca947d2bf0b2ebb118c80e46a9dc2e3e
DineshKumar <49865911+kattadinesh@users.noreply.github.com>
2022-04-23 02:39:15 -0400

    Add files via upload

--- clustering/app.R
+++ clustering/app.R
@@ -1,57 +1,42 @@
-library(shiny)
+#Position based Clustering
 
-data<- read.csv("C:/Users/T460S/Desktop/SDM II/Project/simplified_dataset_v2.csv")
-head(data,10)
-#scaled_data = scale(data[5:7])
-#data[5:7] = scaled_data
-encode_ordinal <- function(x, order = unique(x)) {
-  x=as.numeric(factor(x, levels = order, exclude = NULL))
-  x
-}
+df =  read.csv("C:/Users/T460S/Desktop/data.csv")
+unique_position = list("RE", "CB","HB", "QB", "WR", "LE", "RG",  
+              "TE",  "MLB", "LOLB", "DT", "LT", "SS", "C",
+              "LG", "FS",  "RT", "ROLB", "K", "FB", "P")
 
-table(data[['Player_Name']], encode_ordinal(data[['Player_Name']]), useNA = "ifany")
-data['Player_Name'] = encode_ordinal(data[['Player_Name']])
-table(data[['Position']], encode_ordinal(data[['Position']]), useNA = "ifany")
-data['Position'] = encode_ordinal(data[['Position']])
-data <- subset (data, select = -c(Player_Team_Zone ))
-head(data,20)
+#for (i in unique){
+#   print(filter(data, Position == i))
+#}
 
-vars <- setdiff(names(data),'Team')
+position_data = filter(df, Position == 'K')
+vars = list("Player_Name", "Overall_Rating", "Age","Annual_Salary")
 
-ui <- pageWithSidebar(
-  headerPanel(' k-means clustering'),
-  sidebarPanel(
-    selectInput('xcol', 'X Variable', vars),
-    selectInput('ycol', 'Y Variable', vars, selected = vars[[1]]),
-    numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
-  ),
-  mainPanel(
-    plotOutput('plot1')
-  )
-)
+#UI for the Clustering
+ui = pageWithSidebar(
+     headerPanel(' k-means clustering based on a Position'),
+     sidebarPanel(selectInput('xcol', 'X Variable', vars),
+                  selectInput('ycol', 'Y Variable', vars),
+                  numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)),
+     mainPanel(plotOutput('plot1')))
 
-server <-function(input, output, session) {
+
+# Server
+server = function(input, output, session)
+  
+  {
   
   # Combine the selected variables into a new data frame
-  selectedData <- reactive({
-    data[, c(input$xcol, input$ycol)]
-  })
+  selectedData = reactive({position_data[, c(input$xcol, input$ycol)]})
   
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
-  })
+  clusters = reactive({kmeans(selectedData(), input$clusters)})
   
-  output$plot1 <- renderPlot({
-    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-              "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
-    
-    par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
-  })
+  output$plot1 = renderPlot({palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3","#FF7F00", 
+                                       "#FFFF33", "#A65628", "#F781BF", "#999999"))
+                             par(mar = c(5.1, 4.1, 0, 1))
+                             plot(selectedData(),col = clusters()$cluster,pch = 20, cex = 3)
+                             points(clusters()$centers, pch = 4, cex = 4, lwd = 4)})
   
-}
+  }
 
 shinyApp(ui = ui, server = server)
